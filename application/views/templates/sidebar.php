ea6f65ddf049ea544ba809195d5a055320371cda
Fauzannaufalr <fauzannaufalr@gmail.com>
2023-02-02 17:02:49 +0700

    update sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,36 +1,26 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4" style="
-    background-color: #ff0000;
-">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #ff0000;">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link">
+    <a href="<?= base_url('dashboard') ?>" class="brand-link" style="color: #ffffff;">
         <img src="<?= base_url() ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">HRIS</span>
+            class="brand-image img-circle elevation-3">
+        <h3>HRIS</h3>
     </a>
-
     <!-- Sidebar -->
-    <div class="sidebar">
-
-
-        <!-- SidebarSearch Form -->
-        <div class="form-inline">
-
-        </div>
-
+    <div class="sidebar mt-2">
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="<?= base_url('dashboard') ?>" class="nav-link">
+                    <a href="<?= base_url('dashboard') ?>" class="nav-link" style="color: #ffffff;">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="" class="nav-link">
+                    <a href="" class="nav-link" style="color: #ffffff;">
                         <i class="nav-icon fas fa-database"></i>
                         <p>
                             Master Data
@@ -77,7 +67,7 @@
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="#" class="nav-link" style="color: #ffffff;">
                         <i class="nav-icon fas fa-user-edit"></i>
                         <p>
                             Performances
@@ -86,35 +76,36 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="<?= base_url('Performences/penilaiankinerja') ?>" class="nav-link active">
+                            <a href="<?= base_url('Performences/penilaiankinerja') ?>" class="nav-link "
+                                style="background-color: #ffffff; color: black;">
                                 <p>Penilaian Kinerja</p>
                             </a>
                         </li>
                     </ul>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                 <p>Penilaian Kuesioner</p>
                             </a>
                         </li>
                     </ul>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                 <p>Akumulasi Keseluruhan</p>
                             </a>
                         </li>
                     </ul>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                 <p>Soal Kuesioner</p>
                             </a>
                         </li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="#" class="nav-link" style="color: #ffffff;">
                         <i class="nav-icon fas fa-chalkboard-teacher"></i>
                         <p>
                             Training
@@ -123,28 +114,28 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                 <p>Pelatihan</p>
                             </a>
                         </li>
                     </ul>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                 <p>Dokumentasi</p>
                             </a>
                         </li>
                     </ul>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                 <p>Nilai</p>
                             </a>
                         </li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="#" class="nav-link" style="color: #ffffff;">
                         <i class="nav-icon fas fa-money-bill"></i>
                         <p>
                             Payroll
@@ -153,7 +144,7 @@
                     </a>
                     <ul class="nav nav-treeview nav-sidebar nav-child-indent">
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="#" class="nav-link" style="background-color: #ffffff; color: black;">
                                 <p>
                                     Setting Pajak
                                     <i class="right fas fa-angle-right"></i>
@@ -161,14 +152,14 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link active">
+                                    <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                         <p>Data Pajak</p>
                                     </a>
                                 </li>
                             </ul>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link active">
+                                    <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                         <p>Pajak Karyawan</p>
                                     </a>
                                 </li>
@@ -177,7 +168,7 @@
                     </ul>
                     <ul class="nav nav-treeview nav-sidebar nav-child-indent">
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="#" class="nav-link" style="background-color: #ffffff; color: black;">
                                 <p>
                                     Setting BPJS Kesehatan
                                     <i class="right fas fa-angle-right"></i>
@@ -185,14 +176,14 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link active">
+                                    <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                         <p>Data BPJS</p>
                                     </a>
                                 </li>
                             </ul>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link active">
+                                    <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                         <p>BPJS Karyawan</p>
                                     </a>
                                 </li>
@@ -201,21 +192,21 @@
                     </ul>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                 <p>Perhitungan Gaji</p>
                             </a>
                         </li>
                     </ul>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                 <p>Contract Mitra</p>
                             </a>
                         </li>
                     </ul>
                     <ul class="nav nav-treeview nav-sidebar nav-child-indent">
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="#" class="nav-link" style="background-color: #ffffff; color: black;">
                                 <p>
                                     Laporan
                                     <i class="right fas fa-angle-right"></i>
@@ -223,14 +214,14 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link active">
+                                    <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                         <p>Laporan Gaji Karyawan</p>
                                     </a>
                                 </li>
                             </ul>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link active">
+                                    <a href="#" class="nav-link " style="background-color: #ffffff; color: black;">
                                         <p>Laporan Mitra</p>
                                     </a>
                                 </li>
@@ -239,7 +230,7 @@
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="#" class="nav-link" style="color: #ffffff;">
                         <i class="nav-icon fas fa-user-plus"></i>
                         <p>
                             Recruitment
@@ -248,42 +239,47 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="<?= base_url('Recruitment/kelola') ?>" class="nav-link active">
+                            <a href="<?= base_url('Recruitment/kelola') ?>" class="nav-link "
+                                style="background-color: #ffffff; color: black;">
                                 <p>Kelola Pekerjaan</p>
                             </a>
                         </li>
                     </ul>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="<?= base_url('berkas') ?>" class="nav-link active">
+                            <a href="<?= base_url('berkas') ?>" class="nav-link "
+                                style="background-color: #ffffff; color: black;">
                                 <p>Berkas Pelamar</p>
                             </a>
                         </li>
                     </ul>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="<?= base_url('jadwal') ?>" class="nav-link active">
+                            <a href="<?= base_url('jadwal') ?>" class="nav-link "
+                                style="background-color: #ffffff; color: black;">
                                 <p>Jadwal Interview</p>
                             </a>
                         </li>
                     </ul>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="<?= base_url('soal') ?>" class="nav-link active">
+                            <a href="<?= base_url('soal') ?>" class="nav-link "
+                                style="background-color: #ffffff; color: black;">
                                 <p>Soal Test Pelamar</p>
                             </a>
                         </li>
                     </ul>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="<?= base_url('lamaran') ?>" class="nav-link active">
+                            <a href="<?= base_url('lamaran') ?>" class="nav-link "
+                                style="background-color: #ffffff; color: black;">
                                 <p>Penilaian & Laporan Recruitment</p>
                             </a>
                         </li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="#" class="nav-link" style="color: #ffffff;">
                         <i class="nav-icon fas fa-sign-out-alt"></i>
                         <p>Logout</p>
                     </a>
