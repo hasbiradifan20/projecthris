639a8a4b0773daa60b766555288034afc9bb3bf0
hasbiradifan20 <122733319+hasbiradifan20@users.noreply.github.com>
2023-02-03 09:32:48 +0700

    meng-edit tabel kuesioner dan dashboard

--- application/controllers/master/SoalKuesioner.php
+++ application/controllers/master/SoalKuesioner.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Soal_Kuesioner extends CI_Controller
+class SoalKuesioner extends CI_Controller
 {
     public function __construct()
     {
