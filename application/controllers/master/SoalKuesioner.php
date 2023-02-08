55aabebe277bbd74099d93b7f58183b4c2ee7c28
hasbiradifan20 <122733319+hasbiradifan20@users.noreply.github.com>
2023-02-08 09:18:21 +0700

    memper aiki soal kuesioner

--- application/controllers/master/SoalKuesioner.php
+++ application/controllers/master/SoalKuesioner.php
@@ -6,7 +6,7 @@ class SoalKuesioner extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('DataPosisi_model');
+        $this->load->model('SoalKuesioner_model');
 
     }
 
